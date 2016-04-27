ae56e381b3be1e59eeaed0e5c8d351112fa1600c
shannan <sbunch@outlook.com>
2016-04-27 03:54:57 +0000

    add project page

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 var express = require('express');
 var app = express();
+var templates = require('express-handlebars');
 var port = process.env.PORT || 3000;
 
-app.get('/', function(request,response) {
-    response.send('hi there');
+app.set('views', 'views');
 
+app.engine('hbs', templates({
+  extname: 'hbs',
+  defaultLayout: 'main',
+  layoutsDir: './views/layouts'
+}));
+
+app.set('view engine', 'hbs');
+
+app.use(express.static('public'));
+
+app.get('/', function(request,response) {
+    response.render('home');
+});
+
+app.get('/projects', function(request,response) {
+    response.render('newproject');
 });
 
 app.listen(port, function(){
     console.log('Server is running on ' + port);
 
-})
+});
